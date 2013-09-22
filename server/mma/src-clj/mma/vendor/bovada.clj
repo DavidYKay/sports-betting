8b4741750c0d304a4b8d61d5109fa81f4c628f2c
David Young-Chan Kay <davidykay@gmail.com>
2013-09-21 20:34:25 -0400

    now performing a very crude exact-string name search for bovada odds.

--- server/mma/src-clj/mma/vendor/bovada.clj
+++ server/mma/src-clj/mma/vendor/bovada.clj
@@ -7,11 +7,12 @@
         [mma.vendor.common])
   )
 
-(defn get-odds [fight]
+
+(defn get-all-odds []
   (binding [clj-http.core/*cookie-store* (clj-http.cookies/cookie-store)]
     (let [body (html-string (:body (client/get "http://sports.bovada.lv/sports-betting/mixed-martial-arts.jsp")))
           event-schedule  (html/select body [:#event-schedule])
-          raw-names (map html/text (html/select body [:div.competitor-name-props :span.left]))
+          raw-names (map trim (map html/text (html/select body [:div.competitor-name-props :span.left])))
           names (filter (fn [n]
                           (if (or (substring? #".*aired.*" n)
                                   (substring? #".*Live.*" n))
@@ -22,3 +23,25 @@
           ]
       (partition 2 (map vector names odds))
       )))
+
+(defn get-odds [fight]
+
+)
+
+(defn get-odds-for-fighter [n]
+  (let [all-odds (get-all-odds)
+        fighter-matches? (fn [fighter] (let [fighter-name (first fighter)]
+                                         (if (= fighter-name n)
+                                           true
+                                           false)))
+        filtered-odds (filter (fn [fight]
+                                (let [fighter-a (fighter-matches? (first fight))
+                                      fighter-b (fighter-matches? (last fight))
+                                      result (filter #(not (nil? %)) [fighter-a fighter-b])
+                                      final-result (first (remove false? result))
+                                      ]
+                                  final-result))
+                              all-odds)
+        delta (- (count all-odds) (count filtered-odds))]
+    filtered-odds
+    ))
