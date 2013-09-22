66efaf10510da9fcf71ae4051a25db4d975e3f93
David Young-Chan Kay <davidykay@gmail.com>
2013-09-22 13:52:43 -0400

    created a shitty search page

--- server/mma/src-clj/mma/core.clj
+++ server/mma/src-clj/mma/core.clj
@@ -33,42 +33,48 @@ div-foo
 
   (GET "/fight-details" {{fighter-a-name :fighter-a
                           fighter-b-name :fighter-b} :params}
-       (let [fighters [(crawler/get-fighter fighter-a-name)
-                       (crawler/get-fighter fighter-b-name)]
-             fight {:fighters fighters}
-             should-i-bet (engine/should-i-bet? fight)
-             ]
-         (println "Bet reccos: " should-i-bet)
-         (println "Picture a: " (:picture (first fighters)))
-         (println "Picture b: " (:picture (last fighters)))
+       (if (or (nil? fighter-a-name)
+               (nil? fighter-b-name))
          (html5
-           [:head
-            [:title "Fight Detail Page"]
-            (include-css (str "/css/bootstrap.css"))
-            (include-js (str "/js/bootstrap.js"))
-            ]
-           [:div {:class "row"}
-            [:div {:class "span6"}
+           [:h1 "Search for a fighter"]
+           [:form
+            [:input {:type "text" :name "dish-name" :value ""}]
+            [:input {:type "submit" :name "sa" :value "Search"}]])
 
-             [:h1 fighter-a-name]
-             ;[:img {:src "http://www.fightersgeneration.com/np6/char/magneto-mvc3.jpg" :width 400 :height 600 }]
-             [:img {:src (:picture (first fighters)) :width 400 :height 600 }]
+         (let [fighters [(crawler/get-fighter fighter-a-name)
+                         (crawler/get-fighter fighter-b-name)]
+               fight {:fighters fighters}
+               should-i-bet (engine/should-i-bet? fight)
+               ]
+           (println "Bet reccos: " should-i-bet)
+           (html5
+             [:head
+              [:title "Fight Detail Page"]
+              (include-css (str "/css/bootstrap.css"))
+              (include-js (str "/js/bootstrap.js"))
+              ]
+             [:div {:class "row"}
+              [:div {:class "span6"}
 
-             [:h3 (str "Current Betting odds: " (:a (:odds should-i-bet)))]
-             [:h3 (str "Estimated Win Percentage: " (:a (:prediction should-i-bet)))]
-             ]
-            [:div {:class "span6"}
-             [:h1 fighter-b-name]
-             ;[:img {:src "http://static.giantbomb.com/uploads/original/3/34651/1814401-juggernaut.png" :width 400 :height 600 }]
-             [:img {:src (:picture (last fighters)) :width 400 :height 600 }]
-             [:h3 (str "Current Betting odds: " (:b (:odds should-i-bet)))]
-             [:h3 (str "Estimated Win Percentage: " (:b (:prediction should-i-bet)))]
-             ]
-            ]
-           [:button "Test"]
-           [:ul
-            (for [x (range 10)]
-              [:li x])])))
+               [:h1 fighter-a-name]
+               ;[:img {:src "http://www.fightersgeneration.com/np6/char/magneto-mvc3.jpg" :width 400 :height 600 }]
+               [:img {:src (:picture (first fighters)) :width 400 :height 600 }]
+
+               [:h3 (str "Current Betting odds: " (:a (:odds should-i-bet)))]
+               [:h3 (str "Estimated Win Percentage: " (:a (:prediction should-i-bet)))]
+               ]
+              [:div {:class "span6"}
+               [:h1 fighter-b-name]
+               ;[:img {:src "http://static.giantbomb.com/uploads/original/3/34651/1814401-juggernaut.png" :width 400 :height 600 }]
+               [:img {:src (:picture (last fighters)) :width 400 :height 600 }]
+               [:h3 (str "Current Betting odds: " (:b (:odds should-i-bet)))]
+               [:h3 (str "Estimated Win Percentage: " (:b (:prediction should-i-bet)))]
+               ]
+              ]
+             [:button "Test"]
+             [:ul
+              (for [x (range 10)]
+                [:li x])]))))
 
   (route/resources "/")
   )
