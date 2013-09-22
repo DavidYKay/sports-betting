39edb41c8bf98c199eef11b37b268af763f32762
David Young-Chan Kay <davidykay@gmail.com>
2013-09-22 08:34:25 -0400

    added basic code to consume sentiment endpoint

---
+++ server/mma/src-clj/mma/vendor/sentiment.clj
@@ -0,0 +1,22 @@
+(ns mma.vendor.sentiment
+  (:require [net.cgrand.enlive-html :as html]
+            [clj-http.client :as client]
+            [clojure.string :as string]
+            )
+  (:use [clojure.string :only [split trim]]
+        [mma.vendor.common]
+        [clojure.data.json :only [read-str]]
+        [ring.util.codec :only [url-encode]]
+        )
+  )
+
+(defn get-sentiment [fighter-name]
+  (let [response (client/get (str "http://1.crumbsbot.appspot.com/tweetsentiments?q=" (url-encode fighter-name)))
+        ]
+    (if (< (:status response) 300)
+      (let [json (clojure.walk/keywordize-keys (read-str (:body response)))
+            cleaned (conj json {:score (read-string (:score json))})]
+        cleaned)
+      {:error "Failed to fetch sentiment"
+       :status  (:status response)}
+      )))
